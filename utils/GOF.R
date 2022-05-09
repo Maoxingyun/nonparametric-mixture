cbfcd26d48be2722966642cda374bfc31da554a7
Maoxingyun <mxy123@sjtu.edu.cn>
2022-05-10 00:09:51 +0800

    Add KS test of GOF

--- utils/GOF.R
+++ utils/GOF.R
@@ -9,7 +9,9 @@
 #'  
 #' @export
 library('vsgoftest')
-GOF = function(X, all_dist, cluster_lab){
+
+## KL Divergence
+GOF_KL = function(X, all_dist, cluster_lab){
   
   cluster_hash <- new.env() ## <cluster_lab, data points>
   for(i in 1:max(cluster_lab)){
@@ -42,6 +44,54 @@ GOF = function(X, all_dist, cluster_lab){
       if (result$p.value > pvalue) {
         pvalue = result$p.value
         dist_hash[[as.character(i)]] <- list(name = all_dist[[as.character(j)]], parameter = result$estimate)
+      }
+    }
+  }
+  return(dist_hash)
+}
+
+
+
+
+## Kolmogorov-Smirnov Test
+GOF_KS = function(X, all_dist, cluster_lab){
+  
+  cluster_hash <- new.env() ## <cluster_lab, data points>
+  for(i in 1:max(cluster_lab)){
+    single_X = X[which(cluster_lab==i)]
+    cluster_hash[[as.character(i)]] <- single_X
+  }
+  
+  
+  dist_hash <- new.env() ## <cluster_lab, distribution> 
+  ## distribution is a list, name = "distribution name", parameter = c(parametric vector)
+  for(i in 1:max(cluster_lab)){
+    data = cluster_hash[[as.character(i)]]
+    significant_level = 0.05
+    pvalue = -1
+    
+    ## process data, leave only positive number
+    processed_data = data[data>0]
+    
+    ## fit the distribution
+    for (j in 1:length(all_dist)) {
+      if (all_dist[[as.character(j)]] == "norm") {
+        mle = vs.test(x = data, "dnorm", extend = TRUE, relax = TRUE)
+        result = ks.test(x = data, "pnorm", mle$estimate[1], mle$estimate[2])
+      } else if (all_dist[[as.character(j)]] == "lnorm") {
+        mle = vs.test(x = data, "dlnorm", extend = TRUE, relax = TRUE)
+        result = ks.test(x = data, "plnorm", mle$estimate[1], mle$estimate[2])
+      } else if (all_dist[[as.character(j)]] == "exp") {
+        mle = vs.test(x = data, "dexp", extend = TRUE, relax = TRUE)
+        result = ks.test(x = data, "pexp", mle$estimate[1])
+      } else if (all_dist[[as.character(j)]] == "gamma") {
+        mle = vs.test(x = data, "dgamma", extend = TRUE, relax = TRUE)
+        result = ks.test(x = data, "pgamma", mle$estimate[1], mle$estimate[2])
+      }
+      
+      if (result$p.value > pvalue) {
+        pvalue = result$p.value
+        dist_hash[[as.character(i)]] <- list(name = all_dist[[as.character(j)]], parameter = mle$estimate)
       }
     }
   }
